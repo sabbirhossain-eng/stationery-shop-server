33b3b1698358b42e21a6a2b26cace0e6d9b82633
Sabbir Hossain <sabbirh8181@gmail.com>
2024-11-28 23:29:34 +0600

    Order file and order schema model create

---
+++ src/module/order/order.model.ts
@@ -0,0 +1,30 @@
+import { model, Schema, Types } from "mongoose";
+
+const orderSchema = new Schema({
+    email: {type: String, required: [true, 'Please provide your email'], unique:true,
+        validate:{
+            validator: function (value:string) {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+            },
+            message:'{VALUE} is Invalid email address format',
+        }
+    },
+    product:{
+        type: Types.ObjectId,
+        required: true,
+    },
+    quantity:{
+        type: Number,
+        min: 1,
+        default: 1,
+        required: true,
+    },
+    totalPrice:{
+        type: Number,
+        min: 0,
+        required: true,
+    }
+})
+
+const Order = model('Order', orderSchema)
+export default Order;
