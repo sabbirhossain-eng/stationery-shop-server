63f690059b9b21a4a52aaaf8122587464d8b9793
Sabbir Hossain <sabbirh8181@gmail.com>
2024-11-28 23:47:10 +0600

    Stationery product interface create and code format

--- src/module/stationeryProduct/stationeryProduct.model.ts
+++ src/module/stationeryProduct/stationeryProduct.model.ts
@@ -1,43 +1,53 @@
-import { model, Schema } from "mongoose";
+import { model, Schema } from 'mongoose'
 
-const stationeryProductSchema =new Schema({
+const stationeryProductSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'Please provide The name of the product'],
+      type: String,
+      required: [true, 'Please provide The name of the product'],
     },
-    brand:{
-        type: String,
-        required: [true, 'Please provide The Brand Name of the product'],
+    brand: {
+      type: String,
+      required: [true, 'Please provide The Brand Name of the product'],
     },
-    price:{
-        type: Number,
-        required: [true, 'Please provide The price of the product'],
-        min: 0,
+    price: {
+      type: Number,
+      required: [true, 'Please provide The price of the product'],
+      min: 0,
     },
-    category:{
-        type: String,
-        enum:{
-           values:['Writing', 'Office Supplies', 'Art Supplies', 'Educational', 'Technology'],
-            message:'{VALUE} is not valid, please provide a valid category'
-        },
-        required: true,
+    category: {
+      type: String,
+      enum: {
+        values: [
+          'Writing',
+          'Office Supplies',
+          'Art Supplies',
+          'Educational',
+          'Technology',
+        ],
+        message: '{VALUE} is not valid, please provide a valid category',
+      },
+      required: true,
     },
-    description:String,
-    quantity:{
-        type: Number,
-        min:0,
-        required: [true, 'Please provide The quantity of the product, minimum quantity 0'],
+    description: String,
+    quantity: {
+      type: Number,
+      min: 0,
+      required: [
+        true,
+        'Please provide The quantity of the product, minimum quantity 0',
+      ],
     },
-    inStock:{
-        type:Boolean,
-        required: [true, 'Please provide The stock of the product'],
+    inStock: {
+      type: Boolean,
+      required: [true, 'Please provide The stock of the product'],
     },
-    
-},
-{
+  },
+  {
     timestamps: true,
-})
+  }
+)
 
-const Product = model("Product", stationeryProductSchema)
+const Product = model('Product', stationeryProductSchema)
 
-export default Product;
+export default Product
